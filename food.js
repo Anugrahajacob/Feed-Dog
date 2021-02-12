ced5e669d787d7b854fb199bf35bcceb60f50db6
Anugrahajacob <75684857+Anugrahajacob@users.noreply.github.com>
2021-02-12 21:05:36 +0530

    Add files via upload

---
+++ food.js
@@ -0,0 +1,47 @@
+class food{
+    constructor(){
+        this.foodstock = 0;
+        this.lastfed
+        this.image=loadImage("Images/Milk.png");
+    }
+    updatefoodstock(foodstock){
+        this.foostock=this.foostock
+    }
+
+    GetLastFed(lastfed){
+        this.lastfed=lastfed;
+
+    }
+
+    deductFood(){
+        if(this.foodstock>0){
+            this.foodstock=this.foodstock-1;
+
+        }
+    }
+   getFoodStock(){
+       return this.foodstock 
+   }
+
+      display(){
+          var x = 80
+          var y = 100
+        image(this.image,720,220,70,70);
+        for(var i = 0; i<this.foodstock;i++){
+            if(i%10 === 0){
+                x = 80 
+                y = y+50
+
+            }
+
+            image(this.image,x,y,50,50)
+            x=x+30;
+            
+        }
+        }
+
+
+
+
+    }
+
